78b7700b15271c7ce6d788f6990941451ac154d3
Иванов Артемий Валерьевич <a.ivanov@bftcom.com>
2022-01-10 15:18:39 +0300

    Simple bot classes + Lombok

--- src/main/java/com/temimo/App.java
+++ src/main/java/com/temimo/App.java
@@ -1,18 +1,5 @@
 package com.temimo;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.temimo.entry.HH;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 public class App {
 
     public static void main(String[] args) {
